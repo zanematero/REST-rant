1bea1c7198eac62758118a8ade0086431a47e873
Zane Matero <materozane@gmail.com>
2023-02-12 11:32:26 -0800

    Fixed places typo

--- controllers/places.js
+++ controllers/places.js
@@ -1,22 +1,25 @@
-const router = require('express').Router()
-const React = require('react')
+const router = require('express').Router();
 
-// GET /places
-app.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
-      }]      
-    rres.render('places/index', { places })
-  })  
+router.get('/', (req, res) => {
+	let places = [
+		{
+			name: 'H-Thai-ML',
+			city: 'Seattle',
+			state: 'WA',
+			cuisines: 'Thai, Pan-Asian',
+			pic: 'http://placekitten.com/250/250',
+		},
+		{
+			name: 'Coding Cat Cafe',
+			city: 'Phoenix',
+			state: 'AZ',
+			cuisines: 'Coffee, Bakery',
+			pic: 'http://placekitten.com/250/250',
+		},
+	];
 
-module.exports = router
+	res.render('places/index', {places});
+});
+
+module.exports = router;
+
