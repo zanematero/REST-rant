01fa424e59729c9a105ba03cf4e0d27ce210e0b2
Zane Matero <materozane@gmail.com>
2023-02-13 14:43:32 -0800

    add css

--- views/default.jsx
+++ views/default.jsx
@@ -5,12 +5,15 @@ function Def (html) {
         <html>
             <head>
                 <title>Title</title>
+                <link rel="stylesheet" href="/style.css"/>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"/>
             </head>
             <body>
                 {html.children}
             </body>
         </html>
     )
-  }  
+}
 
 module.exports = Def
+
--- views/home.jsx
+++ views/home.jsx
@@ -4,12 +4,21 @@ const Def = require('./default')
 function home () {
     return (
       <Def>
-          <main>
-              <h1>HOME</h1>
-          </main>
-      </Def>
-    )
-  }
+        <main>
+          <h1>HOME</h1>
+            <div>
+              <img src="/images/food.jpg" width="600" height="400" alt="Food on toast" />
+              <div>
+                Photo by <a href="https://unsplash.com/@ellaolsson">Ella Olsson</a> on <a href="https://unsplash.com/">Unsplash</a>
+              </div>
+            </div>
+          <a href="/places">
+            <button className="btn-primary">Places Page</button>
+          </a>
+        </main>
+    </Def>
+  )
+}
   
 
 module.exports = home
