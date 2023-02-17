85f3a15e2a39d9a4804494849f2bd9772d1ec851
Zane Matero <materozane@gmail.com>
2023-02-16 23:58:31 -0800

    fixed image model routes

--- models/places.js
+++ models/places.js
@@ -3,11 +3,11 @@ module.exports = [{
     city: 'Seattle',
     state: 'WA',
     cuisines: 'Thai, Pan-Asian',
-    pic: '/h-thai-ml-tables.jpg'
+    pic: 'public/images/H-Thai-ML.jpg'
 }, {
     name: 'Coding Cat Cafe',
     city: 'Phoenix',
     state: 'AZ',
     cuisines: 'Coffee, Bakery',
-    pic: '/coffee-cat.jpg'
+    pic: 'public/images/Cat_Cafe.jpg'
 }]
