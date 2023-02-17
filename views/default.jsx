6971d0ec6d07261b8a796e9a8e32b36c5c1e97a7
Zane Matero <materozane@gmail.com>
2023-02-16 22:19:11 -0800

    Navbar and style

--- views/default.jsx
+++ views/default.jsx
@@ -9,6 +9,19 @@ function Def (html) {
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"/>
             </head>
             <body>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        <li>
+                            <a href="/places">Places</a>
+                        </li>
+                        <li>
+                            <a href="/places/new">Add Place</a>
+                        </li>
+                    </ul>
+                </nav>
                 {html.children}
             </body>
         </html>
