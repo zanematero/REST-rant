66f439d0ada4f718c2a971c1d5636a9187241efb
Zane Matero <materozane@gmail.com>
2023-02-19 23:19:06 -0800

    added mongoose

--- models/places.js
+++ models/places.js
@@ -1,17 +1,12 @@
-module.exports = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/H-Thai-ML.jpg',
-    photographer: 'Steve Sharp',
-    url: 'https://unsplash.com/@sharp3'
-}, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/Cat_Cafe.jpg',
-    photographer: 'Tamara Malaniy',
-    url: 'https://unsplash.com/@tamarushphotos'
-}]
+const mongoose = require('mongoose')
+
+const placeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  pic: String,
+  cuisines: { type: String, required: true },
+  city: { type: String, default: 'Anytown' },
+  state: { type: String, default: 'USA' },
+  founded: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema)
